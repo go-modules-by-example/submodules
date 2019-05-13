56ad34e87f3359a8dd4c781941829322edcf0ad6
myitcv <myitcv@example.com>
2019-05-13 15:09:29 +0000

    Add package a

---
+++ a/a.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/go-modules-by-example/submodules/b"
+	"fmt"
+)
+
+const Name = b.Name
+
+func main() {
+	fmt.Println(Name)
+}
---
+++ a/go.mod
@@ -0,0 +1,5 @@
+module github.com/go-modules-by-example/submodules/a
+
+go 1.12
+
+require github.com/go-modules-by-example/submodules/b v0.1.1
